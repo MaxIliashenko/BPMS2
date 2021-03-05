25e6dc36393537838957d67769da8deabc909643
Max Iliashenko <uncle.ho.max@gmail.com>
2021-03-05 14:44:21 -0500

    Added static Home Menu

---
+++ Shared/Layout/HomeItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BPMS.Shared.Layout
+{
+    public class HomeItem
+    {
+        public Guid Id { get; set; }
+        public bool Enabled { get; set; }
+        public bool Root { get; set; }
+        public bool TopMenu { get; set; }
+        public string Text { get; set; }
+        public Guid ParentId { get; set; }
+        public string TextPosition { get; set; }
+        public string TextColor { get; set; }
+        public string TextColorHover { get; set; }
+        public string TextColorHighlight { get; set; }
+        public bool HasChildren { get; set; }
+        public bool IsMenu { get; set; }
+        public string ImageUrl { get; set; }
+        public string IconShape { get; set; }
+        public bool IsFontAwesome { get; set; }
+        public string FAIcon { get; set; }
+    }
+}
